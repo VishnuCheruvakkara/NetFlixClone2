ed80879598e715af5a0818194ba37e0197beedab
vishnu cheruvakkara <cheruvakkarav@gmail.com>
2025-01-01 18:40:45 +0530

    Banner Design on home page completed!

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
 import './Navbar.css'
+import { FaSearch } from "react-icons/fa";
+import { FaRegBell } from "react-icons/fa";
+import { IoMdArrowDropdown } from "react-icons/io";
+import profile_icon from '../../assets/profile-icon-netflix.jpg'
+
 
 function Navbar() {
-  return (
-    <div className="navbar">
-      
-    </div>
-  )
+    return (
+        <div className="navbar">
+            <div className="navbar-left">
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png' alt="" />
+                <ul>
+                    <li>Home</li>
+                    <li>TV Show</li>
+                    <li>Movies</li>
+                    <li>New & Popular</li>
+                    <li>My List</li>
+                    <li>Browse by languages</li>
+                </ul>
+            </div>
+            <div className="navbar-right">
+                <FaSearch className='icons' />
+                <p>Children</p>
+                <FaRegBell className='icons' />
+                <div className="navbar-profile">
+                    <img src={profile_icon} className="profile" alt="" />
+                    <IoMdArrowDropdown className="" />
+                    <div className="dropdown">Sighn out</div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Navbar
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import './Home.css'
-const Home = () => {
-    return (
-        <div className='home'> 
+import Navbar from '../../components/Navbar/Navbar'
+import HeroBanner from '../../assets/BannerImage.jpg'
+import { IoMdInformationCircleOutline } from "react-icons/io";
+import { FaPlay } from "react-icons/fa";
 
+function Home() {
+    return (
+        <div className='home'>
+            <Navbar />
+            <div className='hero'>
+                <img src={HeroBanner} alt="" className='banner-img' />
+                <div className="hero-caption">
+                    <h2>Unlimited movies, TV shows and more</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae amet obcaecati nisi eaque consequatur nobis autem ipsa, culpa doloribus esse</p>
+                    <div className='hero-btns'>
+                        <button className='btn'><FaPlay />Play</button>
+                        <button className='btn dark-btn'><IoMdInformationCircleOutline className='info'/>More Info</button>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
+
+export default Home
